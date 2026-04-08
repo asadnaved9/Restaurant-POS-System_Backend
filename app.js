34c95f9176cce113493c72bcadef702b3ada3a88
Asad Naved <asadnaved4@gmail.com>
2026-04-08 13:56:14 +0530

    Error Handling

--- app.js
+++ app.js
@@ -2,14 +2,21 @@ require("dotenv").config();// by this only .env file be used
 const express = require("express");
 const app = express();
 const connectDB = require("./config/database");
+const config = require("./config/config");
+const globalErrorHandler = require("./middlewares/globalErrorHandler");
+const createHttpError = require("http-errors");
 
-const PORT = process.env.PORT;
+const PORT = config.port;
 connectDB();
 
-
+// Root End Point
 app.get("/", (req, res) => {
     res.json({message : "Hello From POS server!"});
 })
+
+
+// Global Error Handler
+app.use(globalErrorHandler);
 
 app.listen(PORT, () => {
     console.log(`POS Server is listening on PORT ${PORT}`);
