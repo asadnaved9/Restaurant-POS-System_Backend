e59af633ad3c664e0570d78d2e60c2850a666d4a
Asad Naved <asadnaved4@gmail.com>
2026-04-09 23:25:17 +0530

    User Schema

---
+++ models/userModels.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function (v) {
+                return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+            },
+            message: "Please enter a valid email address"
+        }
+    },
+    phone: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function (v) {
+                return /^\d{10}$/.test(v);
+            },
+            message: "Please enter a valid phone number"
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
